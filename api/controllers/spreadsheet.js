df3855f586711a049c961515b7af40b154dd9644
Cristian Ruiz <cristianruiz@Cristians-MacBook-Pro.local>
2019-10-18 23:47:36 -0500

    reading an excel and parsing to json is working

--- api/controllers/spreadsheet.js
+++ api/controllers/spreadsheet.js
@@ -1,6 +1,10 @@
-
-
+const xlsx = require('xlsx');
+const path = require('path');
 
 exports.upload_student_ss = function(req, res) {
-    res.send('Upload Student Spreadsheet Action Here...');
+    var student_data = xlsx.readFile(path.resolve('../Sample Data/CFG - Student Data.xlsx'));
+    var sheet = student_data.Sheets['Students'];
+
+    var student_json = xlsx.utils.sheet_to_json(sheet);
+    res.send('<pre>' + JSON.stringify(student_json, null, 2) + '</pre>');
 }
